cddad8c5092948ff4fc8ea27798b11f6c9b0320d
Matt Gattis <gattis@gmail.com>
2018-10-24 14:42:56 -0400

    updated buildroot

--- ohmbase/package/python-cython/python-cython.mk
+++
@@ -1,6 +0,0 @@
-PYTHON_CYTHON_VERSION = master
-PYTHON_CYTHON_SITE = https://github.com/cython/cython.git
-PYTHON_CYTHON_SITE_METHOD = git
-PYTHON_CYTHON_SETUP_TYPE = setuptools
-
-$(eval $(host-python-package))
